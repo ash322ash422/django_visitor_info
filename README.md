bcd20d2280b9288fc979918961df51251971ac60
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2024-06-22 10:56:47 +0530

    summary

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # django-visitor-ip
 Get Visitor info in Django Project
 
-I am using python3.12.2 The packages that were installed: django, geoip2, django-user-agents
+1)I am using python3.12.2 The packages that were installed: django, geoip2, django-user-agents
 
-NOTE: The contents of dir django_visitor_info/VISITOR_INFO/geoip:
+2) The contents of dir django_visitor_info/VISITOR_INFO/geoip:
 
 .DS_Store
 
@@ -16,3 +16,6 @@ GeoLite2-Country.mmdb
 GeoLite2-Country_20191008.tar.gz
 
 #########################################################
+3) Run this using: python.exe .\manage.py runserver 0.0.0.0:8000
+
+Now you find ip of your machine and URL into it "<url_here>:8000"
--- VISITOR_INFO/app/views.py
+++ VISITOR_INFO/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.gis.geoip2 import GeoIP2
+import geoip2.errors
 from datetime import datetime
 
 def home(request):
@@ -9,12 +10,6 @@ def home(request):
         ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
-    
-    #NOTE Following is band-aid fix for error "AddressNotFoundError at /. The address 127.0.0.1 is not in the database."
-    if ip == '127.0.0.1': 
-        print("NNNOTE: USING MOCK IP SINCE YOU ARE RUNNING THIS LOCALLY.")
-        #ip = "185.172.110.206"
-        ip = "108.174.10.10"
     
     device_type = ""
     browser_type = ""
@@ -29,17 +24,27 @@ def home(request):
     if request.user_agent.is_pc:
         device_type = "PC"
     
+    message = ""
     browser_type = request.user_agent.browser.family
     browser_version = request.user_agent.browser.version_string
     os_type = request.user_agent.os.family
     os_version = request.user_agent.os.version_string
-    
     g = GeoIP2()
-    location = g.city(ip)
+    
+    #NOTE Following is band-aid fix for error "AddressNotFoundError at /. The address 127.0.0.1 is not in the database."
+    try:
+        location = g.city(ip)
+    except geoip2.errors.AddressNotFoundError:
+        mock_ip = "108.174.10.10"
+        location = g.city(mock_ip) #use dummy ip then
+        message = "NNNOTE-> USING MOCK IP SINCE YOU ARE RUNNING THIS LOCALLY."
+        #print(message)
+        
     location_country = location["country_name"]
     location_city = location["city"]
 
     context = {
+        "message": message,
         "ip": ip,
         "device_type": device_type,
         "browser_type": browser_type,
