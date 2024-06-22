0c838f93212c9989221e497155b5080d17376d33
ash322ash422 <160450942+ash322ash422@users.noreply.github.com>
2024-06-22 09:41:43 +0530

    summary

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# django_visitor_info
- 
+# django-visitor-ip
+Get Visitor IP Address in Django Project
+
+I am using python3.12.2
+NOTE: The content of dir geoip:
+.DS_Store
+
+GeoLite2-City.mmdb
+
+GeoLite2-City_20191008.tar.gz
+
+GeoLite2-Country.mmdb
+
+GeoLite2-Country_20191008.tar.gz
---
+++ VISITOR_INFO/app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, HttpResponse
+from django.contrib.gis.geoip2 import GeoIP2
+
+def home(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    #NOTE Following is band-aid fix for error "AddressNotFoundError at /. The address 127.0.0.1 is not in the database."
+    if ip == '127.0.0.1': 
+        print("NNNOTE: USING MOCK IP SINCE YOU ARE RUNNING THIS LOCALLY.")
+        #ip = "185.172.110.206"
+        ip = "108.174.10.10"
+    
+    device_type = ""
+    browser_type = ""
+    browser_version = ""
+    os_type = ""
+    os_version = ""
+
+    if request.user_agent.is_mobile:
+        device_type = "Mobile"
+    if request.user_agent.is_tablet:
+        device_type = "Tablet"
+    if request.user_agent.is_pc:
+        device_type = "PC"
+    
+    browser_type = request.user_agent.browser.family
+    browser_version = request.user_agent.browser.version_string
+    os_type = request.user_agent.os.family
+    os_version = request.user_agent.os.version_string
+    
+    g = GeoIP2()
+    location = g.city(ip)
+    location_country = location["country_name"]
+    location_city = location["city"]
+
+    context = {
+        "ip": ip,
+        "device_type": device_type,
+        "browser_type": browser_type,
+        "browser_version": browser_version,
+        "os_type":os_type,
+        "os_version":os_version,
+        "location_country": location_country,
+        "location_city": location_city
+    }
+
+    return render(request, "home.html", context)
